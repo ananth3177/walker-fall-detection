cc1795b159c8a779190299d79be3b7a533c1d044
ananth3177 <ananth3@purdue.edu>
2025-12-04 04:07:13 -0500

    all code updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "secrets.h"
-#include <TFT_eSPI.h>
-#include <Arduino.h>
-TFT_eSPI tft;
+#include "display.h"
+#include "sensors.hpp"
+#include "feedback.hpp"
+#define BUTTON_PIN 27 //gpio button trigger for sensor test
+const uint32_t SMS_COOLDOWN_MS = 5UL * 60UL * 1000UL; //5 mins
+uint32_t lastSmsMs = 0;
 
-#define BUTTON_PIN 33 //gpio button trigger for sensor test
+
 
 void sendIFTTT(String msg){
+
     if(WiFi.status() == WL_CONNECTED){
         HTTPClient http;
 
@@ -25,12 +29,13 @@ void sendIFTTT(String msg){
         else{
             Serial.println("Wi-Fi not connected");
         }   
-    
+       
 }
 
 void setup(){
+    //pinMode(BUTTON_PIN, INPUT_PULLUP);
     Serial.begin(115200);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    
 
     //connect wifi
     // Serial.print("ssid=");
@@ -42,15 +47,60 @@ void setup(){
         Serial.println(".");
     }
     Serial.println("\nWi-Fi connected!");
+    display_setup();
+
+    sensors_loop();
+
+    feedback_setup();
 
 }
 
-void loop(){
-    if(digitalRead(BUTTON_PIN)== LOW) {
-        Serial.println("Button pressed -> sending SMS");
-        sendIFTTT("Button pressed - sending SMS");
-        delay(3000);
+// void loop(){
+//     uint32_t now = millis();
+//     int cur = digitalRead(BUTTON_PIN);
+//     if(lastBtn == HIGH && cur == LOW ) {
+//         Serial.print("Edge detected at ms =");
+//         Serial.println(now);
+//         if((now - lastFireMs) > COOLDOWN_MS){
+//             Serial.println("Button pressed -> sending SMS");
+//             sendIFTTT("Button pressed - sending SMS");
+//             lastFireMs = now;
+//         } else{
+//             Serial.println("ignored still in cooldown");
+//         }
+//     }
+//     lastBtn = cur;
+
+
+//     display_loop();
+//     delay(10);
+
+// }
+
+void loop() {
+    static bool prevAlert = false;
+
+    display_loop();
+    feedback_update();
+    bool nowAlert = display_in_alert();
+    uint32_t now = millis();
+
+    if (nowAlert && !prevAlert) {
+        Serial.println("Entered ALERT State");
+        if(now - lastSmsMs > SMS_COOLDOWN_MS){
+            Serial.println("Sending SMS via IFTTT...");
+            sendIFTTT("Walker has fallen");
+            lastSmsMs = now;
+        } else{
+            Serial.println("SMS supressed (software cooldown)");
+        }
     }
+    
+    if(!nowAlert && prevAlert) {
+        Serial.println("Exited ALERT state");
+    }
+                                                                    
+    prevAlert = nowAlert;
 
+    delay(10);
 }
-
