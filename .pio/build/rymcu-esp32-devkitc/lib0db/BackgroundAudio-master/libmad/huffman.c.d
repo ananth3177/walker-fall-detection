e25af950a62d006358c58ff0f4e99240f353db00
ananth3177 <ananth3@purdue.edu>
2025-12-05 17:02:51 -0500

    initial commit on new repo

--- .pio/build/rymcu-esp32-devkitc/lib0db/BackgroundAudio-master/libmad/huffman.c.d
+++
@@ -1,6 +0,0 @@
-.pio/build/rymcu-esp32-devkitc/lib0db/BackgroundAudio-master/libmad/huffman.c.o: \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/huffman.c \
- C:/Users/1705s/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/config.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/global.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/huffman.h
--- .pio/build/rymcu-esp32-devkitc/lib0db/BackgroundAudio-master/libmad/stream.c.d
+++
@@ -1,7 +0,0 @@
-.pio/build/rymcu-esp32-devkitc/lib0db/BackgroundAudio-master/libmad/stream.c.o: \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/stream.c \
- C:/Users/1705s/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/config.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/global.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/bit.h \
- .pio/libdeps/rymcu-esp32-devkitc/BackgroundAudio-master/src/libmad/stream.h
